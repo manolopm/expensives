175aa87a55acd44b42392360f8e67e4bf873b5ed
Manuel Padrón Martínez <manolopm@gmail.com>
2013-02-03 16:27:04 +0000

    Change precision in qty

--- db/migrate/20130203162349_create_entries.rb
+++ db/migrate/20130203162349_create_entries.rb
@@ -2,7 +2,7 @@ class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
       t.string :concept
-      t.decimal :quantity
+      t.decimal :quantity, :precision => 8, :scale => 2
       t.boolean :recurse_monthly
       t.boolean :recurse_yearly
       t.date :date
